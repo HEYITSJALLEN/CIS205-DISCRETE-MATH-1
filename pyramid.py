77d428fd5902bc4b393a5bc171ba1406182cef77
HEYITSJALLEN <jallenwenceslao@gmail.com>
2024-10-10 22:11:07 -1000

    practice

---
+++ pyramid.py
@@ -0,0 +1,7 @@
+rows = int(input("Enter the number of rows: "))
+for i in range(1, rows + 1):
+    for space in range(1, (rows - i) + 1):
+        print(end=" ")
+    for star in range(1, 2 * i):
+        print("*", end="")
+    print()
