474db0b3fea6d590ce6731f421ea2bbc1e7d12c1
HEYITSJALLEN <jallenwenceslao@gmail.com>
2024-10-16 15:27:28 -1000

    practice

---
+++ oct10.py
@@ -0,0 +1,12 @@
+# Demostrate list comprehension
+
+# Create a list of squares
+squares = [i**2 for i in range(10)]
+
+squares2 = []
+for x in range(10):
+    squares2.append(x**2)
+
+# Print the list
+print(squares)  # Output: [0, 1, 4, 9,]
+
