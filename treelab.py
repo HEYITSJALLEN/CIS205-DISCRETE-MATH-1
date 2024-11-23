ca775bd13e302ee34b6b8b43e5fec645d921e908
HEYITSJALLEN <jallenwenceslao@gmail.com>
2024-11-22 14:47:55 -1000

    practice

--- treelab.py
+++ treelab.py
@@ -16,8 +16,26 @@ for item in inputList:
     # if the next item is a number, push it onto the stack.
     if item.isnumeric() == True: # ----> .isnumeric is a method or property
         myStack.append(item)
+    else:
+        num1 = myStack.pop()
+        num2 = myStack.pop()
+    # if the next item is an operator, pop the last two items
+    #from the stack and store them as num1 and num2
+        if item == "+":
+            result = num1 + num2
+        elif item == "-":
+            result = num1 - num2
+        elif item == "*":
+            result = num1 * num2
+        elif item == "/":
+            result = num1 / num2
 
-print(myStack)
+            # Can't multiply sequence by non-int of type 'str'
+    
+
+# print(myStack)
+# myStack.pop()
+# print(myStack)
     #     print("YES")
     # else:
     #     print ("NO")
